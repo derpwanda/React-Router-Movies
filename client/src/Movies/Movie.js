4f323185cc188515ba773aadb8c4658084b00553
Wanda <doubleuevans@gmail.com>
2018-10-23 12:32:29 -0500

    refactored MovieList and MovieCard. How refactor Movie?

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -27,6 +27,7 @@ export default class Movie extends Component {
         console.error(error);
       });
   };
+  
   // Uncomment this code when you're ready for the stretch problems
   // componentWillReceiveProps(newProps){
   //   if(this.props.match.params.id !== newProps.match.params.id){
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -3,7 +3,26 @@ import React from 'react';
 function MovieCard(props) {
   // const id = props.match.params.id;
   // const movie = movies.find(movie => `${movie.id}`)
-  return;
+  const { title, director, metascore, stars } = props.movie;
+
+  return (
+    <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+  );
 };
 
 export default MovieCard;
