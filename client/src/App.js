b84feda701347f356803254084cd2d6892a28bd0
derpWanda <derpWanda@gmail.com>
2019-09-07 15:14:49 -0500

    save now working.  added line 36

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,43 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
 
-import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      savedList: []
+    constructor() {
+        super();
+        this.state = {
+            savedList: []
+        };
+    }
+
+    addToSavedList = movie => {
+        const savedList = this.state.savedList;
+        savedList.push(movie);
+        this.setState({ savedList });
     };
-  }
 
-  addToSavedList = movie => {
-    const savedList = this.state.savedList;
-    savedList.push(movie);
-    this.setState({ savedList });
-  };
-
-  render() {
-    return (
-      <div>
-        <SavedList list={this.state.savedList} />
-        <Route exact path="/" render={props => <MovieList {...props} />} />
-        <Route path="/movies/:id" render={props => <Movie {...props} />} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <SavedList list={this.state.savedList} />
+                <Route
+                    exact
+                    path='/'
+                    render={props => <MovieList {...props} />}
+                />
+                <Route
+                    path='/movies/:id'
+                    render={props => (
+                        <Movie
+                            {...props}
+                            addToSavedList={this.addToSavedList}
+                        />
+                    )}
+                />
+            </div>
+        );
+    }
 }
